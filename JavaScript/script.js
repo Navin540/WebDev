9f33c9fcbf7a35a5590e647f6dba12886f9d877f
navinkumar <navinkumar.tem@gmail.com>
2023-02-17 08:26:52 +0530

    added JS file

---
+++ JavaScript/script.js
@@ -0,0 +1,12 @@
+// alert('welcome to js');
+console.log("welcome");
+console.log([50,26,96,55]);
+console.log({fname:'navin',age:25});
+console.table({fname:'navin',age:25});
+console.error("custom error");
+console.time("Timer")
+for(i=0;i<10;i++){
+    console.log(i);
+}
+console.timeEnd("Timer");
+
